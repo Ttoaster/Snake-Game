75896b81e3cf0ba30a7dde7f18ee78a72071b66e
Richard Lowe <66838438+Ttoaster@users.noreply.github.com>
2022-02-14 15:30:37 -0500

    updated js file

--- snake.js
+++ snake.js
@@ -21,17 +21,17 @@ class Snake {
                 y: this.tail[this.tail.length - 1].y
             }
         } else if  (this.rotateX == -1) {
-             newRectangle = {
+            newRectangle = {
                 x: this.tail[this.tail.length - 1].x - this.size,
                 y: this.tail[this.tail.length - 1].y
             }
         } else if (this.rotateY == 1) {
-             newRectangle = {
+            newRectangle = {
                 x: this.tail[this.tail.length - 1].x,
                 y: this.tail[this.tail.length - 1].y + this.size
             }
         } else if (this.rotateY == -1) {
-             newRectangle = {
+            newRectangle = {
                 x: this.tail[this.tail.length - 1].x,
                 y: this.tail[this.tail.length - 1].y - this.size
             }
